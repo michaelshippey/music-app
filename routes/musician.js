2224e57d909b00597ad20f78ab7a184156ec5e51
michaelshippey <michaelshippey@u.boisestate.edu>
2020-10-19 12:10:32 -0700

    health checks added

--- routes/musician.js
+++ routes/musician.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const schema = require('../store/schema');
 
+
+// retrieve all musicians from data store
+router.get('/health', (req, res) => {
+    res.status('200').send("Status okay!");
+});
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
